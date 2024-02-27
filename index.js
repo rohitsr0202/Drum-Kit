aa699a56662cad49246371cdac96915edc6039ad
Rohit Singh <139547743+rohitsr0202@users.noreply.github.com>
2024-02-27 11:10:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,101 @@
+//                    higher order function
+// function add(num1, num2) {
+//     return num1 + num2;
+//     }
+     
+//     function subtract(num1, num2) {
+//     return num1 - num2;
+//     }
+     
+//     function multiply(num1, num2) {
+//     return num1 * num2;
+//     }
+     
+//     function divide(num1, num2) {
+//     return num1 / num2;
+//     }
+     
+//     function calculator(num1, num2, operator) {
+//     return operator(num1, num2);
+//     }
+
+
+
+
+
+
+
+
+
+
+
+// for click sound
+var drumLength = document.querySelectorAll(".drum").length;
+
+for(var i=0; i < drumLength; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        
+        var buttonInerHtml = this.innerHTML;
+        
+        makeSound(buttonInerHtml);
+        buttonAnimation(buttonInerHtml);
+
+    });
+}
+
+
+// for keybord button sound
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+
+});
+
+
+function makeSound(key){
+
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "k":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "l":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+    
+        default:
+            break;
+    }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(() => {
+    activeButton.classList.remove("pressed");
+        
+    }, 100);
+}
